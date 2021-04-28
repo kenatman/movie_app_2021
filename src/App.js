876072ff431614e4948359fbb728136b3dedc904
Yunsu <kenatman0723@gmail.com>
2021-04-28 13:20:14 +0900

    동적 component 만들기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h4>I like {fav}</h4>;
+function Food({ menu, picture }) {
+  return (
+    <div>
+      <h4>I like {menu}</h4>
+      <img src={picture} alt={menu} />
+    </div>
+  );
 }
+
+const foodArray = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGtpbWNoaXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    name: "Bibimbab",
+    image:
+      "https://images.unsplash.com/photo-1590301157890-4810ed352733?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmliaW1iYXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    name: "pizza",
+    image:
+      "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGl6emF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    name: "pasta",
+    image:
+      "https://images.unsplash.com/photo-1590477413464-ce102b59852c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fHBhc3RhfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="samkyupsal" />
-      <Food fav="chukumi" />
-      <Food fav="chicken" />
+      {foodArray.map((food) => (
+        <Food key={food.id} menu={food.name} picture={food.image} />
+      ))}
     </div>
   );
 }
