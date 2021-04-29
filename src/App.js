cd9fa102afdf001891191c2a0dd53a29ec88e2a7
Yunsu <kenatman0723@gmail.com>
2021-04-29 12:52:16 +0900

    initializing Movie App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React from "react";
 
 class App extends React.Component {
-  state = { count: 0 };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+  state = { isLoading: true, movies: [] };
 
   componentDidMount() {
-    console.log("Component rendered first");
-  }
-
-  componentDidUpdate() {
-    console.log("Im updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, Component");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
   }
 
   render() {
-    console.log("Im rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "Ready!!!"}</div>;
   }
 }
 
