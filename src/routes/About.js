d3da31060dbf017bcd0e16d41c7107c74a949e07
Yunsu <kenatman0723@gmail.com>
2021-05-03 11:19:16 +0900

    Navigation with {Link to}

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span> This is About page!!!</span>;
+  return (
+    <div className="about__container">
+      <span>Overthinking is the biggest cause of unhappiness.</span>
+      <span>-GAUTAMA BUDDHA</span>
+    </div>
+  );
 }
 
 export default About;
